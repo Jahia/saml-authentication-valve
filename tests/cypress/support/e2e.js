5eb3f8013e624b2509c5cada71ead1ed3219f6e0
Geofrey Flores <gflores@jahia.com>
2023-03-13 21:08:30 -0400

    Add embedded video/screenshot on mocha reports

--- tests/cypress/support/e2e.js
+++ tests/cypress/support/e2e.js
@@ -40,3 +40,15 @@ if (Cypress.browser.family === 'chromium') {
         params: {cacheDisabled: true}
     });
 }
+
+// add embeddable video/screenshot with mocha report
+Cypress.on('test:after:run', (test, runnable) => {
+    let videoName = Cypress.spec.name;
+    videoName = videoName.replace('/.cy.*', '');
+    const videoUrl = 'videos/' + videoName + '.mp4';
+    addContext({test}, videoUrl);
+    if (test.state === 'failed') {
+        const screenshot = `screenshots/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`;
+        addContext({test}, screenshot);
+    }
+});
