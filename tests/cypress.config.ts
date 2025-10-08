8785a949e7aadfd370d1ed567a841ffc73a2b032
Jérôme Blanchard <jblanchard@jahia.com>
2025-10-08 16:10:25 +0200

    Add video to tests

--- tests/cypress.config.ts
+++ tests/cypress.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'cypress';
 
 export default defineConfig({
+    video: true,
     chromeWebSecurity: false,
     defaultCommandTimeout: 30000,
     reporter: 'cypress-multi-reporters',
