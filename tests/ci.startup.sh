5f7eb8695f16efd55d4c08efc98382a16ea9f259
Francois G. <fgerthoffert@jahia.com>
2026-06-22 10:14:08 +0200

    use common scripts

--- tests/ci.startup.sh
+++ tests/ci.startup.sh
@@ -1,32 +1,18 @@
-#!/usr/bin/env bash
-
-# This script controls the startup of the container environment
-# It can be used as an alternative to having docker-compose up started by the CI environment
+#!/bin/bash
 
 source ./set-env.sh
 
 echo " == Printing the most important environment variables"
 echo " MANIFEST: ${MANIFEST}"
+echo " NEXUS_USERNAME: ${NEXUS_USERNAME}"
 echo " TESTS_IMAGE: ${TESTS_IMAGE}"
 echo " JAHIA_IMAGE: ${JAHIA_IMAGE}"
+echo " JAHIA_CLUSTER_ENABLED: ${JAHIA_CLUSTER_ENABLED}"
+echo " MODULE_ID: ${MODULE_ID}"
 echo " JAHIA_URL: ${JAHIA_URL}"
+echo " SUPER_USER_PASSWORD: ${SUPER_USER_PASSWORD}"
+echo " SMTP_SERVER_URL: ${SMTP_SERVER_URL}"
 
-docker-compose pull jahia
-
-# WORKAROUND: It seems keycloak could not reach ldap server the first time its started
-# even with a delay between ldap server and keycloak
-# Workaround is to start it the first time and let it error out, remove the container and restart again.
-# docker-compose up -d keycloak
-# sleep 45
-# docker stop keycloak; docker rm keycloak
-# sleep 15
-
-docker-compose up -d --renew-anon-volumes --remove-orphans --force-recreate jahia
-
-if [[ $1 != "notests" ]]; then
-    echo "$(date +'%d %B %Y - %k:%M') [TESTS] == Starting cypress tests =="
-    docker-compose up --abort-on-container-exit --renew-anon-volumes cypress
-fi
-
-# echo " == Printing keycloak server logs"
-# docker logs keycloak
+version=$(node -p "require('./package.json').devDependencies['@jahia/cypress']")
+echo Using @jahia/cypress@$version...
+npx --yes --package @jahia/cypress@$version ci.startup
